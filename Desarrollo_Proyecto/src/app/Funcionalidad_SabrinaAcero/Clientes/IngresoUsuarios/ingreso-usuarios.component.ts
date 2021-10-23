c237f9b990b3a9cd591a10bc4abce6a3bbc67dfc
Sabci3 <sabci3@example.com>
2021-10-22 22:04:00 -0500

    Funcionalidad 1

---
+++ Desarrollo_Proyecto/src/app/Funcionalidad_SabrinaAcero/Clientes/IngresoUsuarios/ingreso-usuarios.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ingreso-usuarios',
+  templateUrl: './ingreso-usuarios.component.html',
+  styleUrls: ['./ingreso-usuarios.component.css']
+})
+export class IngresoUsuariosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
